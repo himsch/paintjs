4fc1f8b26271b22d98e0dc471ee2f343b9f225ae
himsch <55rsearchco@gmail.com>
2020-12-23 01:47:22 +0900

    Finish save update

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 
 const INITIAL_COLOR = "#2c2c2c";
 const CANVAS_SIZE = 700;
@@ -10,6 +11,8 @@ const CANVAS_SIZE = 700;
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
 
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
@@ -17,67 +20,86 @@ ctx.lineWidth = 2.5;
 let painting = false;
 let filling = false;
 
-function stopPainting(){
-    painting = false;
+function stopPainting() {
+  painting = false;
 }
 
-function startPainting(){
-    painting = true;
+function startPainting() {
+  painting = true;
 }
 
-function onMouseMove(event){
-    const x = event.offsetX;
-    const y = event.offsetY;
-    if(!painting){
-        ctx.beginPath();    // path은 선이다.
-        ctx.moveTo(x, y);
-    } else {
-        ctx.lineTo(x, y);   // 마우스에 위치에서 현재위치까지 선을 만듬 / 마우스를 움직이느 내낸 사용되는 함수
-        ctx.stroke();       // 선을 긋는 함수 / 마우스를 움직이는 내내 사용되는 함수
-    }
+function onMouseMove(event) {
+  const x = event.offsetX;
+  const y = event.offsetY;
+  if (!painting) {
+    ctx.beginPath(); // path은 선이다.
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y); // 마우스에 위치에서 현재위치까지 선을 만듬 / 마우스를 움직이느 내낸 사용되는 함수
+    ctx.stroke(); // 선을 긋는 함수 / 마우스를 움직이는 내내 사용되는 함수
+  }
 }
 
-function handleColorClick(event){
-    const color = event.target.style.backgroundColor;
-    ctx.strokeStyle = color;
-    ctx.fillStyle = color;
+function handleColorClick(event) {
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+  ctx.fillStyle = color;
 }
 
-function handleRangeChange(event){
-    const size = event.target.value;
-    ctx.lineWidth = size;
+function handleRangeChange(event) {
+  const size = event.target.value;
+  ctx.lineWidth = size;
 }
 
-function handleModeClick(){
-    if(filling) {
-        filling = false;
-        mode.innerText = "FILL";
-    } else {
-        filling = true;
-        mode.innerText = "PAINT";
-    }
+function handleModeClick() {
+  if (filling) {
+    filling = false;
+    mode.innerText = "FILL";
+  } else {
+    filling = true;
+    mode.innerText = "PAINT";
+  }
 }
 
-function handleCanvasClick(){
-    if(filling){
-        ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
-    }
+function handleCanvasClick() {
+  if (filling) {
+    ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+  }
 }
 
-if(canvas){
-    canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", startPainting);
-    canvas.addEventListener("mouseup", stopPainting);      // 드래그하다 놓으면 페인팅 false
-    canvas.addEventListener("mouseleave", stopPainting);
-    canvas.addEventListener("click", handleCanvasClick);
+function handleCM(event) {
+  event.preventDefault(); // 마우스 오른쪽클릭으로 이미지저장 금지
 }
 
-Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
-
-if(range) {
-    range.addEventListener("input", handleRangeChange);
+function handleSaveClick() {
+  const image = canvas.toDataURL();
+  const link = document.createElement("a");
+  link.href = image;
+  link.download = "PaintJS[✨]";
+  link.click();
 }
 
-if(mode){
-    mode.addEventListener("click", handleModeClick)
+if (canvas) {
+  canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting); // 드래그하다 놓으면 페인팅 false
+  canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("click", handleCanvasClick);
+  canvas.addEventListener("contextmenu", handleCM);
+}
+
+Array.from(colors).forEach(color =>
+  color.addEventListener("click", handleColorClick)
+);
+
+if (range) {
+  range.addEventListener("input", handleRangeChange);
+}
+
+if (mode) {
+  mode.addEventListener("click", handleModeClick);
+}
+
+if (saveBtn) {
+  saveBtn.addEventListener("click", handleSaveClick);
 }
